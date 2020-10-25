afcf7513c936cac164d8a9d45311294fe5c659b3
Alexandre VOGEL <avogel@tour.home>
2020-10-25 21:23:07 +0100

    add test

--- serveur/test/check.sh
+++ serveur/test/check.sh
@@ -3,7 +3,16 @@ if ! test -x ./bin/serveur; then
   exit 1
 fi
 
+echo "test 1"
 if echo "stop" | ./bin/serveur 4242 test.conf; then
+  printf "\033[32mSUCCESS\033[0m\n"
+else
+  printf "\n\033[31mFAIL\033[0m\n"
+  exit 1
+fi
+
+echo "test 2"
+if (sleep 10 && echo "stop") | ./bin/serveur 4242 toto > /dev/null & sleep 1 && nc -u 127.0.0.1 4242 < /proc/cpuinfo > /dev/null & sleep 1 && nc -u ::1 4242 < /proc/cpuinfo > /dev/null & wait ; then
   printf "\033[32mSUCCESS\033[0m\n"
   exit 0
 else
